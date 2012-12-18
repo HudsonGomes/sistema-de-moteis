09a1be82d81d30e3f999077bca54bebf2b5b8af3
Hudson <hudson.sgomes@gmail.com>
2012-12-18 14:15:58 -0200

    retirando notifier pois deu problema

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,10 @@ module SistemaDeMoteis
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    config.middleware.use ExceptionNotifier,
-    :email_prefix => "[Automático] ERRO SISTEMA DE MOTÉIS ",
-    :sender_address => %{"Error Notifier" hudson.sgomes@gmail.com},
-    :exception_recipients => %w{hudson.sgomes@gmail.com marcogbarcellos@gmail.com}
+#    config.middleware.use ExceptionNotifier,
+#    :email_prefix => "[Automático] ERRO SISTEMA DE MOTÉIS ",
+#    :sender_address => %{"Error Notifier" hudson.sgomes@gmail.com},
+#    :exception_recipients => %w{hudson.sgomes@gmail.com marcogbarcellos@gmail.com}
 
 
     # Activate observers that should always be running.
